c8bf5d9bc9792a159e293b931491557bece240f2
Yitong Deng <thomasdeng1999@gmail.com>
2024-02-29 16:24:37 -0800

    something

--- script.js
+++ script.js
@@ -1,84 +1,70 @@
-function extractAndConcatenateWords(jsonObj) {
+function populate_text(jsonObj, textbox) {
   let concatenatedString = '';
 
-  function traverseAndCollectWords(obj) {
-      if (Array.isArray(obj)) {
-          // If it's an array, iterate through its elements
-          obj.forEach(element => {
-              traverseAndCollectWords(element);
-          });
-      } else if (typeof obj === 'object' && obj !== null) {
-          // If it's an object, iterate through its properties
-          Object.keys(obj).forEach(key => {
-              if (key === 'word' && typeof obj[key] === 'string') {
-                  // If the key matches and its value is a string, concatenate it
-                  concatenatedString += obj[key] + ' '; // Adding a space for separation
-              } else {
-                  // Otherwise, continue traversing
-                  traverseAndCollectWords(obj[key]);
-              }
-          });
+  jsonObj.forEach(obj => {
+    Object.keys(obj).forEach(key => {
+      if (key === 'word' && typeof obj[key] === 'string') {
+          // If the key matches and its value is a string, concatenate it
+          concatenatedString += obj[key] + ' '; // Adding a space for separation
       }
-  }
-
-  // Start traversing the JSON object
-  traverseAndCollectWords(jsonObj);
+    });
+    
+  });
 
   // Return the concatenated string, trimming any trailing space
-  return concatenatedString.trim();
+  textbox.innerHTML = concatenatedString.trim();
 }
 
-function populate(jsonObj) {
-  let concatenatedString = '';
-
-  function traverseAndCollectWords(obj) {
-      if (Array.isArray(obj)) {
-          // If it's an array, iterate through its elements
-          obj.forEach(element => {
-              traverseAndCollectWords(element);
-          });
-      } else if (typeof obj === 'object' && obj !== null) {
-          // If it's an object, iterate through its properties
-          Object.keys(obj).forEach(key => {
-              if (key === 'word' && typeof obj[key] === 'string') {
-                  // If the key matches and its value is a string, concatenate it
-                  concatenatedString += obj[key] + ' '; // Adding a space for separation
-              } else {
-                  // Otherwise, continue traversing
-                  traverseAndCollectWords(obj[key]);
-              }
-          });
-      }
+function populate_table(jsonObj, table) {
+  let idx = 0;
+  jsonObj.forEach(obj => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+        <td>
+        #${idx}
+        </td>
+        <td>
+        ${obj["words"]}
+        </td>
+        <td>
+        ${Math.round(obj["start"]*1000)/1000}
+        </td>
+        <td>
+        ${Math.round(obj["end"]*1000)/1000}
+        </td>`;
+        // You could also do the same for the cells and inputs
+        table.appendChild(row);
+        idx += 1;
+  });
+    
   }
-
-  // Start traversing the JSON object
-  traverseAndCollectWords(jsonObj);
-
-  // Return the concatenated string, trimming any trailing space
-  return concatenatedString.trim();
-}
 
 fetch("http://192.168.50.97:8080/words.json")
   .then(response => response.json())
   .then(data => {
-    obj = extractAndConcatenateWords(data);
-    document.getElementById('transcribed_paragraph').innerHTML = obj;
+    obj = populate_text(data, document.getElementById('transcribed_paragraph'));
    });
 
-  const table = document.getElementById('registerTable');
-  const row = document.createElement('tr');
-  row.innerHTML = `
-  <td>
-  #${1}
-  </td>
-  <td>
-  ${2}
-  </td>
-  <td>
-  (${3}, ${4})
-  </td>`;
-  // You could also do the same for the cells and inputs
-  table.appendChild(row);
+fetch("http://192.168.50.97:8080/segments_processed.json")
+   .then(response => response.json())
+   .then(data => {
+     obj = populate_table(data, document.getElementById('registerTable'));
+    });
+
+  // const table = document.getElementById('registerTable');
+  // const row = document.createElement('tr');
+  // row.innerHTML = `
+  // <td>
+  // #${1}
+  // </td>
+  // <td>
+  // ${2}
+  // </td>
+  // <td>
+  // (${3}, ${4})
+  // </td>`;
+  // // You could also do the same for the cells and inputs
+  // table.appendChild(row);
 
 // // const result = extractAndConcatenateWords(obj);
 // console.log(obj.count); // Output: "Hello World Foo Bar End
